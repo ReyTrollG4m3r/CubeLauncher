97e682932f6658ec0a708e1ca1ac1fd995bc6c9d
ReyTrollG4m3r <105902818+ReyTrollG4m3r@users.noreply.github.com>
2024-08-03 13:31:52 -0400

    Update link.py

---
+++ link.py
@@ -0,0 +1,16 @@
+def obtener_version():
+    return "1.1.0.0"
+
+def obtener_archivos():
+    return [
+        "https://drive.google.com/file/d/1C9fC7Hwp_tUL06C6_hqDLJhm8KouDDcz/view?usp=drive_link",
+        "https://drive.google.com/file/d/1TsK6EEBrGbZ6pEUgaP3zPsZekXPHUn6M/view?usp=drive_link"
+        # Agrega más enlaces aquí si es necesario
+    ]
+
+def obtener_rutas():
+    return [
+        "C:\\usuarios",
+        "C:\\Intel"
+        # Agrega más rutas aquí si es necesario
+    ]
